1c6683b60a11d83d6de533a780e0ead7fd240d01
will <williamr723@gmail.com>
2023-06-07 04:09:17 -0300

    deixando home padrao

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pessoas/', include('appPessoas.urls', namespace='pessoas'))
+    path('', include('appPessoas.urls', namespace='pessoas'))
 ]
